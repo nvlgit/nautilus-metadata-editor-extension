d78bb941bd6c840b6d8f0c0e41844a821ec2ed3f
nvlgit <nvlbox@gmail.com>
2018-06-18 14:02:58 +0300

    Create nautilus-metadata-editor-extension.h

---
+++ src/nautilus/nautilus-metadata-editor-extension.h
@@ -0,0 +1,32 @@
+/* nautilus-metadata-editor-extension.h
+ *
+ * Copyright 2018 Nick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define NAUTILUS_TYPE_METADATAEDITOR  (metadata_editor_get_type ())
+
+G_DECLARE_FINAL_TYPE (MetadataEditor, metadata_editor, NAUTILUS, METADATAEDITOR, GObject)
+
+void metadata_editor_load (GTypeModule *module);
+
+G_END_DECLS
